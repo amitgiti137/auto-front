592bd75d57dab1ad0b852488d85f57c7461efab7
amitgiti137 <amit1317jmp@gmail.com>
2025-03-01 21:57:38 +0530

    Initial commit

--- app/myapp/dashboard.jsx
+++ app/myapp/dashboard.jsx
@@ -19,7 +19,7 @@ export default function Dashboard() {
     }, []); */
 
     return (
-        <div className='bg-[#F0F0D7] h-screen'>
+        <div className='bg-[#F2F9FF] h-screen'>
             {/* <h1>Dashboard</h1>
             {tasks.map((task) => (
                 <div key={task._id}>
